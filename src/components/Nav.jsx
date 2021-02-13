b914406015e5b8bf2094b7ae66421b6de649caca
Aglowkeys <emilianoalfonso@outlook.com>
2021-02-13 15:06:36 -0300

    Corrected some things

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Nav.css';
 import SearchBar from './SearchBar.jsx';
-import MainNavigation from './MainNavigation.jsx'
+import MainNavigation from './MainNavigation.jsx';
 
-
-function Nav({onSearch}) {
-   return (
-      <nav>
-         <div className="container">
-            <h1 className="brand"><Link to='/'>{"{ EWA }"}</Link></h1>
-            <MainNavigation />
-            <SearchBar onSearch={onSearch} />
-         </div>
-      </nav>
-   );
-};
+function Nav({ onSearch }) {
+	return (
+		<nav>
+			<div className='container'>
+				<h1 className='brand'>
+					<Link to='/'>{'{ EWA }'}</Link>
+				</h1>
+				<MainNavigation />
+				<SearchBar onSearch={onSearch} />
+			</div>
+		</nav>
+	);
+}
 
 export default Nav;
